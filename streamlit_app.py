fc00d2c03ac7f7bbea565dd3aa7716ed595451b7
ship123talk456 <158138042+ship123talk456@users.noreply.github.com>
2024-09-13 22:04:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,8 @@ import time
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
     </style>
     """,
     unsafe_allow_html=True
