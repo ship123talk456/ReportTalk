73d3f0afbf68c91389c5b0955cce32534be9534d
ship123talk456 <158138042+ship123talk456@users.noreply.github.com>
2024-09-13 22:20:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.markdown(
     .css-1q1n0ol.egzxvld1 {display: none !important;}
 
     /* Hide the GitHub icon */
-    .st-emotion-cache-q16mip e3g6aar1 {display: none !important;}
+    .st-emotion-cache-1wbqy5l e3g6aar2 {display: none !important;}
     </style>
     """,
     unsafe_allow_html=True
