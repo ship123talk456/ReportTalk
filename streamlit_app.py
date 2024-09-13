e0a63f4b485c26722984a5470f7a8c6247c8dcaa
ship123talk456 <158138042+ship123talk456@users.noreply.github.com>
2024-09-13 22:01:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,18 @@ from email.mime.multipart import MIMEMultipart
 from datetime import datetime
 import time
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # 设置数据库连接
 conn = sqlite3.connect('shipping_system.db')
