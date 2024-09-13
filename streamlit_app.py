3ce0892b0b86ccfa10e76718e152256e0504b77f
ship123talk456 <158138042+ship123talk456@users.noreply.github.com>
2024-09-13 22:30:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.markdown(
     footer {visibility: hidden;}
 
     /* Hides the GitHub icon and Share button */
-    #GithubIcon {visibility: hidden;}
+    #stActionButton {visibility: hidden;}
    
     /* Alternatively, you can set display to none if visibility doesn't work */
     /* header > div:nth-child(2) > button {display: none;} */
